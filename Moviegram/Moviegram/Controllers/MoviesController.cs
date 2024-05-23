cfe3fa4ec942f6bdf85d40d81206860385b8365f
umutcakir10 <ucakir81@gmail.com>
2024-05-23 13:23:18 +0300

    Draft 2 movieapp

---
+++ Moviegram/Moviegram/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moviegram.Repositories;
+
+namespace Moviegram.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IMoviePostRepository moviePostRepository;
+
+        public MoviesController(IMoviePostRepository moviePostRepository)
+        {
+            this.moviePostRepository = moviePostRepository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string urlHandle)
+        {
+            var moviePost = await moviePostRepository.GetByUrlHandleAsync(urlHandle);
+            return View(moviePost);
+        }
+    }
+}
